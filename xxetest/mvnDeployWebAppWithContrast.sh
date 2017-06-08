555c4464a1b38eaf141afcbb8868145b8b6101cf
Dave Wichers <dwichers@gmail.com>
2017-06-08 14:45:14 -0400

    add contrast for unix script. Minor fixes to pom and other batch file.

---
+++ xxetest/mvnDeployWebAppWithContrast.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if [ -f contrast.jar ]; then
+	mvn clean compile war:war cargo:run -PdeployWithContrast
+  else 
+	echo "Contrast is a commercial product, so you need a licensed version of Contrast in order to run it on this application. If you have access to Contrast, download the Contrast Agent for Java (contrast.jar) from the Team Server and put it into the base directory (xxetest), and then rerun this script."
+
+fi
