f8fb9fa95e5d4362456c5363b6963dbf34e2a807
Chris Lyne <cclyne@gmail.com>
2018-02-19 17:15:47 +0000

    Updating run script and xml

--- mvnDeployWebApp.sh
+++ mvnDeployWebApp.sh
@@ -2,4 +2,16 @@
 
 mvn initialize
 ./setExternalSpringConfig.sh # this sets the config for the Spring app
+
+# build and run spring app
+mvn -f spring-xml-parsers/pom.xml clean package
+java -jar spring-xml-parsers/target/spring-xml-parsers-0.1.0.war &
+spring_pid=$!
+trap ctrl_c 2 # kill backgrounded process if ctrl-c is pressed
+ctrl_c() {
+    echo "\n*** Killing Spring PID $spring_pid ***\n"
+    kill $spring_pid
+}
+
 mvn clean compile war:war cargo:run
+
