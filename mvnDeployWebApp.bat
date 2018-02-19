26653b81ce01c24d1edafe74325c0ed4edd89b08
Robert Lyne <lynerc@dukes.jmu.edu>
2018-02-19 14:10:17 -0500

    Port startup scripts to Windows

--- mvnDeployWebApp.bat
+++ mvnDeployWebApp.bat
@@ -1,2 +1,10 @@
 call mvn initialize
+
+REM this sets the config for the Spring app
+call setExternalSpringConfig.bat
+
+REM build and run spring app
+call mvn -f spring-xml-parsers/pom.xml clean package
+start call java -jar spring-xml-parsers/target/spring-xml-parsers-0.1.0.war
+
 call mvn clean compile war:war cargo:run
---
+++ spring-xml-parsers/build_and_run.bat
@@ -0,0 +1,3 @@
+call mvn -f pom.xml clean package
+call java -jar target/spring-xml-parsers-0.1.0.war
+
