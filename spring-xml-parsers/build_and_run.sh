f52468a52dd88e82db364894ff5ea3032f70c52e
Chris Lyne <cclyne@gmail.com>
2018-02-19 15:42:33 +0000

    enable build and run from anywhere

--- spring-xml-parsers/build_and_run.sh
+++ spring-xml-parsers/build_and_run.sh
@@ -1,3 +1,7 @@
-mvn clean package
-java -jar target/spring-xml-parsers-0.1.0.war
+# get running dir so we can run from anywhere
+this_script=`readlink -f $0`
+this_dir=`dirname $0`
+
+mvn -f $this_dir/pom.xml clean package
+java -jar $this_dir/target/spring-xml-parsers-0.1.0.war
 
