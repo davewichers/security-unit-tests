adb91e8c1904f254bbb215eee6c4b8687efd2019
davewichers <dwichers@gmail.com>
2018-10-16 20:08:46 +0000

    Upgrade OWASP Encoder library version and fix to minor typos.

--- mvnDeployWebAppWithContrast.bat
+++ mvnDeployWebAppWithContrast.bat
@@ -3,5 +3,5 @@ if exist .\contrast.jar (
 	call mvn initialize
 	call mvn clean compile war:war cargo:run -PdeployWithContrast
 ) else (
-	echo Contrast is a commercial product, so you need a licensed version of Contrast in order to run it on this application. If you have access to Contrast, download the Contrast Agent for Java (contrast.jar^) from the Team Server and put it into the base directory (xxetest^), and then rerun this script.
+	echo Contrast is a commercial product, so you need a licensed version of Contrast in order to run it on this application. If you have access to Contrast, download the Contrast Agent for Java (contrast.jar^) from the Team Server and put it into the base directory (security-unit-tests^), and then rerun this script.
 )
--- mvnDeployWebAppWithContrast.sh
+++ mvnDeployWebAppWithContrast.sh
@@ -4,6 +4,6 @@ if [ -f contrast.jar ]; then
 	mvn initialize
 	mvn clean compile war:war cargo:run -PdeployWithContrast
   else 
-	echo "Contrast is a commercial product, so you need a licensed version of Contrast in order to run it on this application. If you have access to Contrast, download the Contrast Agent for Java (contrast.jar) from the Team Server and put it into the base directory (xxetest), and then rerun this script."
+	echo "Contrast is a commercial product, so you need a licensed version of Contrast in order to run it on this application. If you have access to Contrast, download the Contrast Agent for Java (contrast.jar) from the Team Server and put it into the base directory (security-unit-tests), and then rerun this script."
 
 fi
