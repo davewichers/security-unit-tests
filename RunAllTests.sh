1094610ce2cb1648c5c2e4fb36d4edb0ec1d6e8f
Dave Wichers <dwichers@gmail.com>
2022-11-14 16:51:51 -0500

    Minor updates to setup instructions and upgrade run all tests script to use Python 3.

--- RunAllTests.sh
+++ RunAllTests.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-pip install -U selenium
+pip3 install -U selenium
 
 if [ -f chromedriver ]; then
 	export PATH=$PATH:./chromedriver
 fi
 
-python WebTestCrawler.py
+python3 WebTestCrawler.py
